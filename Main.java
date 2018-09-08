9cc9261f4965d8cd034a9c950a06f85b1688751c
yesasurya <yesasurya01@gmail.com>
2018-09-08 11:14:58 +0700

    Modifying class Main

--- Main.java
+++ Main.java
@@ -4,9 +4,10 @@ class Main {
 
     public static void main(String[] args) {
         ArrayList<Branch> branches = Reader.getBranchesFromFile("history.txt");
-        for (Branch branch : branches) {
-            System.out.println(branch.isTaken());
-        }
+        BranchPredictor branchPredictor = new BranchPredictor(4, branches);
+        branchPredictor.startPredictionWithInitialPrediction(1);
+        System.out.println("Number of BTB entries: " + branchPredictor.getNumberOfEntries());
+        System.out.println("Misinterpretation rate: " + branchPredictor.getMisinterpretationRate());
     }
 
 }
